e82960dea80f1ebb3404aae1bd44fad3e2a75f4d
Victor Gil <baskeboler@gmail.com>
2022-03-22 02:18:38 -0300

    auth

--- project.clj
+++ project.clj
@@ -9,7 +9,8 @@
                  [duct/module.logging "0.5.0"]
                  [duct/module.sql "0.6.1"]
                  [duct/module.web "0.7.3"]
-                 [org.postgresql/postgresql "42.2.19"]]
+                 [org.postgresql/postgresql "42.2.19"]
+                 [duct/middleware.buddy "0.2.0"]]
   :plugins [[duct/lein-duct "0.12.3"]]
   :main ^:skip-aot bank-service.main
   :resource-paths ["resources" "target/resources"]
